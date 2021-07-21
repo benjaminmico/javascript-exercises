36f2080f96f5d03c131a1f353beb0b84d6d814b7
joffrey <joffrey.berrier@gmail.com>
2021-07-21 16:32:58 +0200

    feat(exercise): add some exercises

---
+++ README.md
@@ -0,0 +1,17 @@
+# Front-end exercice
+
+# Exercices
+
+Pour lancer les tests tu dois ouvrir ton terminal et faire :
+
+```bash
+cd leprojet
+```
+
+Pour lancer les exercices :
+
+```bash
+node exercices/${numéro de l'exercice}/index.js
+```
+
+...
---
+++ exercices/2/index.js
@@ -0,0 +1,21 @@
+function exercice2(arr) {
+  return arr;
+}
+
+/* Résultat attendu =
+[
+  { id: 1, type: "house" },
+  { id: 2, type: "house" },
+  { id: 4, type: "house" },
+];
+*/
+
+console.log(
+  exercice2([
+    { id: 1, type: "house" },
+    { id: 2, type: "house" },
+    { id: 3, type: "destination" },
+    { id: 4, type: "house" },
+    { id: 5, type: "destination" },
+  ])
+);
